0801ffe87c0ee88797d81af2b18e20947dc07284
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-01-09 23:03:01 +0200

    Completes Exercise 2

--- src/es2/Main.java
+++ src/es2/Main.java
@@ -1,4 +1,25 @@
 package es2;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        concatStrings();
+    }
+
+    public static void concatStrings() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci la prima stringa:");
+        String str1 = scanner.nextLine();
+        System.out.println("Inserisci la seconda stringa:");
+        String str2 = scanner.nextLine();
+        System.out.println("Inserisci la terza stringa:");
+        String str3 = scanner.nextLine();
+
+        System.out.println("Le stringhe concatenate sono: " + str1 + str2 + str3);
+        System.out.println("Le stringhe concatenate al contrario sono: " + str3 + str2 + str1);
+
+        scanner.close();
+    }
 }
