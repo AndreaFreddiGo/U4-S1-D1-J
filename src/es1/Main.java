37da781031864a059881c5d62533e4ed3a1e2ca4
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-01-09 22:38:46 +0200

    Completes Exercise 1

--- src/es1/Main.java
+++ src/es1/Main.java
@@ -1,9 +1,13 @@
 package es1;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         multiply(4, 6);
         concat("Ciao", 2020);
+        String[] array = {"a", "b", "c", "d", "e"};
+        insert(array, "f");
     }
 
     public static void multiply(int num1, int num2) {
@@ -14,8 +18,9 @@ public class Main {
         System.out.println("Il risultato della concatenazione è: " + str + num3);
     }
 
-    public static void insert(String str, int num3) {
-        System.out.println("Il risultato della concatenazione è: " + str + num3);
+    public static void insert(String[] stringArray, String str) {
+        String[] newArray = {stringArray[0], stringArray[1], stringArray[2], str, stringArray[3], stringArray[4]};
+        System.out.println("Il nuovo array è: " + Arrays.toString(newArray));
     }
 
 }
